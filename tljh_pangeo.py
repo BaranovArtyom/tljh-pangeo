e4b3a8391a15dc08dbbc2e4aa9a2f014bf1ad7fe
Artem Baranov <artem.brnv@gmail.com>
2024-04-17 23:56:04 +0300

    check

--- tljh_pangeo.py
+++ tljh_pangeo.py
@@ -11,7 +11,7 @@ def tljh_extra_user_conda_packages():
 @hookimpl
 def tljh_config_post_install(config):
     """
-    Set JupyterLab to be default
+    Set JupyterLab to be default!!!
     """
     user_environment = config.get('user_environment', {})
     user_environment['default_app'] = user_environment.get('default_app', 'jupyterlab')
